a7323e551e5f5b92e09fd6069256ff6fce93ea07
petar-kostadinov <153288029+petar-kostadinov@users.noreply.github.com>
2026-03-15 20:29:16 +0200

    config routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,10 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './features/home/home';
+import { Themes } from './features/themes/themes';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: "", redirectTo: "home", pathMatch: "full" },
+    
+    { path: "home", component: HomeComponent },
+    { path: "themes", component: Themes },
+];
--- src/app/app.ts
+++ src/app/app.ts
@@ -2,11 +2,10 @@ import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from './layout/header/header';
 import { FooterComponent } from './layout/footer/footer';
-import { HomeComponent } from './features/home/home';
 
 @Component({
   selector: 'app-root',
-  imports: [HeaderComponent, FooterComponent, HomeComponent],
+  imports: [HeaderComponent, FooterComponent, RouterOutlet],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
