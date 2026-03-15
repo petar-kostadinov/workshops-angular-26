d274acac5a9a1f16135a5f0258278c201a6da40a
petar-kostadinov <153288029+petar-kostadinov@users.noreply.github.com>
2026-03-15 22:32:46 +0200

    add register and login page

--- src/app/features/auth/login/login.ts
+++ src/app/features/auth/login/login.ts
@@ -1,9 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { Router, RouterLink } from "@angular/router";
+import { AuthService } from '../../../core/services/auth';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
-  imports: [],
+  imports: [FormsModule, RouterLink],
   templateUrl: './login.html',
   styleUrl: './login.css',
 })
-export class LoginComponent {}
+export class LoginComponent {
+  private authService = inject(AuthService);
+  private router = inject(Router);
+
+  email = '';
+  password = '';
+
+  onLogin(): void {
+    const succes = this.authService.login(this.email, this.password);
+
+    if (succes) {
+      this.router.navigate(['/themes']);
+    } else {
+      alert('Invalid credentilas');
+    }
+  }
+}
--- src/app/features/auth/register/register.ts
+++ src/app/features/auth/register/register.ts
@@ -1,9 +1,57 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { AuthService } from '../../../core/services/auth';
+import { UserService } from '../../../core/services/user';
+import { User, UserWithCredentials } from '../../../shared/interfaces/user';
+import { Router, RouterLink } from "@angular/router";
 
 @Component({
   selector: 'app-register',
-  imports: [],
+  imports: [FormsModule, RouterLink],
   templateUrl: './register.html',
   styleUrl: './register.css',
 })
-export class RegisterComponent {}
+export class RegisterComponent {
+  private router = inject(Router);
+  private authService = inject(AuthService);
+  private userService = inject(UserService);
+
+  username = '';
+  email = '';
+  tel = '';
+  password = '';
+  repassword = '';
+
+  onRegister(): void {
+    if (!this.email) {
+      alert('Email is requred');
+      return;
+    }
+
+    if (!this.password) {
+      alert('Password is requred');
+      return;
+    }
+
+    if (this.repassword !== this.password) {
+      alert('Password dont match');
+      return;
+    }
+
+    const newUser: UserWithCredentials = {
+      _id: this.generateId(),
+      username: this.username,
+      email: this.email,
+      tel: "+359" + this.tel,
+      password: this.password
+    }
+
+    const sessionUser = this.userService.register(newUser);
+    this.authService.setSession(sessionUser);
+    this.router.navigate(['/themes']);
+  }
+
+  private generateId(): string {
+    return Math.random().toString(36).substring(2, 15);
+  }
+}
