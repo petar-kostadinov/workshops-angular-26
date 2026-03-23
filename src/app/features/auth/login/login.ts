71245603cc6a4257bd7659ebff4769b4392597ab
petar-kostadinov <153288029+petar-kostadinov@users.noreply.github.com>
2026-03-23 23:01:12 +0200

    add login logic

--- src/app/features/auth/login/login.ts
+++ src/app/features/auth/login/login.ts
@@ -1,28 +1,53 @@
 import { Component, inject } from '@angular/core';
-import { Router, RouterLink } from "@angular/router";
+import { Router, RouterLink } from '@angular/router';
 import { AuthService } from '../../../core/services/auth';
-import { FormsModule } from '@angular/forms';
+import { FormBuilder, FormGroup, FormsModule, Validators } from '@angular/forms';
+import { emailValidator } from '../../../shared/validators/email.validator';
+import { InputErrorDirective } from "../../../shared/directives/input-error";
 
 @Component({
   selector: 'app-login',
-  imports: [FormsModule, RouterLink],
+  imports: [FormsModule, RouterLink, InputErrorDirective],
   templateUrl: './login.html',
   styleUrl: './login.css',
 })
 export class LoginComponent {
   private authService = inject(AuthService);
   private router = inject(Router);
+  private fb = inject(FormBuilder);
 
-  email = '';
-  password = '';
+  loginForm: FormGroup = this.fb.group({
+    email: ['', [Validators.required, emailValidator()]],
+    passeord: ['', [Validators.required, Validators.minLength(5)]],
+  });
+
+  isLoading = false;
+  errorMessage = '';
 
   onLogin(): void {
-    const succes = this.authService.login(this.email, this.password);
-
-    if (succes) {
-      this.router.navigate(['/themes']);
-    } else {
-      alert('Invalid credentilas');
+    if (this.loginForm.invalid) {
+      this.loginForm.markAllAsTouched();
+      return;
     }
+
+    this.isLoading = true;
+    this.errorMessage = '';
+
+    const { email, password } = this.loginForm.value;
+
+    this.authService.login({ email, password }).subscribe({
+      next: (user) => {
+        this.isLoading = false;
+        if (user) {
+          this.router.navigate('/themes');
+        } else {
+          this.errorMessage = 'Invalid email or password';
+        }
+      },
+      error: (err) => {
+        this.isLoading = false;
+        this.errorMessage = err.error?.message || 'Login failed. Try again';
+      },
+    });
   }
 }
