a5fe9154b8303038b75c38e1484cae6db8f73fa8
petar-kostadinov <153288029+petar-kostadinov@users.noreply.github.com>
2026-03-25 23:26:27 +0200

    update create form

--- src/app/features/themes/new-theme/new-theme.ts
+++ src/app/features/themes/new-theme/new-theme.ts
@@ -1,22 +1,49 @@
-import { Component, inject } from '@angular/core';
-import { FormsModule } from '@angular/forms';
+import { Component, inject, ViewChild } from '@angular/core';
+import { FormsModule, NgForm } from '@angular/forms';
 import { Router } from '@angular/router';
+import { ApiService } from '../../../core/services/api';
+import { InputErrorDirective } from '../../../shared/directives/input-error';
 
 @Component({
   selector: 'app-new-theme',
-  imports: [FormsModule],
+  imports: [FormsModule, InputErrorDirective],
   templateUrl: './new-theme.html',
   styleUrl: './new-theme.css',
 })
 export class NewThemeComponent {
+  @ViewChild('themeForm') themeForm!: NgForm;
+
   themeName = '';
   postText = '';
+  isLoading = false;
+  errorMessage = '';
 
   private router = inject(Router);
+  private apiService = inject(ApiService);
 
   onSubmit(): void {
-    console.log('Creating theme', this.themeName, this.postText);
-    this.router.navigate(['/themes']);
+    if (this.themeForm.invalid) {
+      return;
+    }
+
+    this.isLoading = true;
+    this.errorMessage = '';
+
+    this.apiService
+      .createTheme({
+        themeName: this.themeName,
+        postText: this.postText,
+      })
+      .subscribe({
+        next: (theme) => {
+          this.isLoading = false;
+          this.router.navigate(['/themes', theme._id]);
+        },
+        error: (err) => {
+        this.isLoading = false;
+        this.errorMessage = err.error?.message || 'Failed to create new theme. Try again';
+      },
+      });
   }
 
   onCancel(): void {
